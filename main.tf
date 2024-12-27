55700c861bf5b7ea266e6a1c3101469b32c1a894
JitendraMaddula <82214202+JitendraMaddula@users.noreply.github.com>
2024-12-27 10:50:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0e2c8caa4b6378d8c" # Replace with your desired AMI ID
   instance_type = "t2.micro"
-  key_name      = "in Mac" # Replace with your SSH key name
+  key_name      = "SShkey" # Replace with your SSH key name
 
   tags = {
     Name = var.aws_instance_name
